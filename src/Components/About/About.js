f06ac759f88bf3577e12b4e4afaaf29efaa5d7a7
developerhr000 <80412774+developerhr000@users.noreply.github.com>
2022-04-05 04:32:05 +0600

    100% complete

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,7 @@ import image from '../../Images/about.jpg'
 const About = () => {
   return (
     <div className="p-10 mx-auto max-w-7xl items-center justify-between px-10 md:px-4">
-        <img src={image} className='' alt="" />
+        <img src={image} className='' alt=""/>
     </div>
   );
 };
